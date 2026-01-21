a72d34c2ac7aadb59e48776ca1e1cb842033cc27
Тагир Гимранов <gimranov.tagir@bk.ru>
2026-01-21 15:57:59 +0500

    add_Test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void firstLogin() {
+        //открыть браузер
+        //зайти на сайт https://www.sausedemo.com/
+
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+
+        browser.quit();
+
+
+    }
+}
