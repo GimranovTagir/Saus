14d41a7efe132331603cfb8f9e8e695fbaec1f97
Тагир Гимранов <gimranov.tagir@bk.ru>
2026-01-24 18:08:12 +0500

    Corrected BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -18,12 +18,13 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("start-maximized");
         //options.addArguments("headless");
-        options. addArguments("--windows-size=1920,1080");
+        options.addArguments("--windows-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
         //browser.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
     }
+
     @AfterMethod
     public void closeBrowser() {
         //browser.quit();
