94ebd081c1d5333ed320effcdc624cb98a60f799
Тагир Гимранов <144915431+GimranovTagir@users.noreply.github.com>
2026-02-02 12:24:59 +0500

    add Cart(Page/Test) fixed pom

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.WebDriver;
 
 import javax.xml.namespace.QName;
 
-public class ProductsPage extends BasePage {
+public class  ProductsPage extends BasePage {
     private static final String ADD_TO_CART_PATTERN =
             "//*[text()='%s']//ancestor::div[@class='inventory_item']//child::button[text()='Add to cart']";
-    private final By title = By.cssSelector("[data-test='title']");
+    private final By title = By.cssSelector(DATA_TEST_PATTERN.formatted("title"));
     private final By cartCounter = By.cssSelector(DATA_TEST_PATTERN.formatted("shopping-cart-badge"));
+    private final By cartLink = By.cssSelector(DATA_TEST_PATTERN.formatted("shopping-cart-link"));
 
 
     public ProductsPage(WebDriver driver) {
@@ -20,13 +21,14 @@ public class ProductsPage extends BasePage {
         return driver.findElement(title).isDisplayed();
     }
 
-    public String getTitle() {
-        return driver.findElement(title).getText();
-    }
-
     public void addGoodsToCarts(String goodsName) {
         By addToCart = By.xpath(ADD_TO_CART_PATTERN.formatted(goodsName));
         driver.findElement(addToCart).click();
+    }
+
+    public void addGoodToCart(int goodsIndex) {
+        driver.findElements(By.xpath("//*[text()='Add to cart']")).get(goodsIndex).click();
+
     }
 
     public String checkCounterValue() {
@@ -35,5 +37,8 @@ public class ProductsPage extends BasePage {
 
     public String checkCounterColorValue() {
         return driver.findElement(cartCounter).getCssValue("background-color");
+    }
+    public void switchToCart() {
+         driver.findElement(cartLink).click();
     }
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -3,18 +3,28 @@ package tests;
 import net.bytebuddy.implementation.EqualsMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class ProductsTest extends BaseTest {
+    List<String> goodList = new ArrayList<>(
+            List.of("Test.allTheThings() T-Shirt (Red)", "Sauce Labs Onesie", "Sauce Labs Fleece Jacket")
+    );
+
     @Test
     public void checkGoodsAdded() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        productPage.isTitleIsDisplayed();
-        productPage.addGoodsToCarts("Sauce Labs Onesie");
-        productPage.addGoodsToCarts("Sauce Labs Fleece Jacket");
-        productPage.addGoodsToCarts("Test.allTheThings() T-Shirt (Red)");
-        assertEquals(productPage.checkCounterValue(), "3");
-        assertEquals(productPage.checkCounterColors(),"rgba(266,34,26,1)");
+        assertEquals(productPage.checkTitleName(), "Products");
+        assertTrue(productPage.isTitleIsDisplayed());
+        for (int i=0; i < goodList.size(); i++){
+            productPage.addGoodsToCarts(goodList.get(i));
+        }
+        productPage.addGoodToCart(2);
+
+        assertEquals(productPage.checkCounterValue(), "4");
+        assertEquals(productPage.checkCounterColorValue(),"rgba(266,34,26,1)");
     }
 }
